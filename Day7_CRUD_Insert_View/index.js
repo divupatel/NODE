1c8fa3c0e526d81f591505c85893b9ce5dee0661
divupatel <divyavaddoriya22@gmail.com>
2024-12-04 05:02:10 +0530

    day 8 4-12-2024

--- Day7_CRUD_Insert_View/index.js
+++ Day7_CRUD_Insert_View/index.js
@@ -1,34 +1,68 @@
 const express = require('express');
 const port = 8000;
-
 const app = express();
-
 const path = require('path');
 
-const studentData = [{name : 'Divya',email : 'divya@gmail.com'}];
+const stData = [
+    {
+        first_name: 'Divya',
+        last_name:'John',
+        email: 'divya@gmail.com',
+        gender: 'Female',
+        age: 26,
+        city:'Surat',
+        hobby: 'Reading',
+        about:'lorem ipsum dolor sit amet, consectetur adipiscing elit'
+
+    },
+    {
+        first_name: 'Srushti',
+        last_name:'John',
+        email: 'srushti@gmail.com',
+        gender: 'Female',
+        age: 26,
+        city:'Surat',
+        hobby: 'Reading',
+        about:'lorem ipsum dolor sit amet, consectetur adipiscing elit'
+
+    }
+]
 
 app.use(express.urlencoded());
 
 app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname,'views'));
-
-
+app.set('views', path.join(__dirname, 'views'));
 app.get('/',(req,res)=>{
     res.render('home',{
-        'data' : studentData
+        stData : stData
     });
 })
 
 app.post('/insertData',(req,res)=>{
-    console.log(req.body);
-    const newStudent = {
-        name: req.body.name,
-        email : req.body.email,
-        age : req.body.age
-    }
-    studentData.push(newStudent);
+    // console.log(req.body);
+    stData.push(req.body);
     res.redirect('/');
+})
 
+app.get('/deleteData',(req,res)=>{
+    stData.splice(req.query.pos,1);
+    res.redirect('/');
+})
+
+app.get('/editData',(req,res)=>{
+    // console.log(req.query);
+    let eData = stData[req.query.pos];
+    res.render('edit',{
+        eData,
+        position : req.query.pos
+    })
+    res.redirect('/');
+})
+
+app.post('/updateData',(req,res)=>{
+    console.log(req.body);
+    stData[req.body.position] = req.body
+    res.redirect('/');
 })
 
 app.listen(port,(err)=>{
@@ -37,4 +71,4 @@ app.listen(port,(err)=>{
         return false;
     }
     console.log(`Server is running at ${port}`);
-})
+});
