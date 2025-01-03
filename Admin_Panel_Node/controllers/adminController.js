e26172bc672a309b5a64c37bf44bf4cd00897dcb
divupatel <divyavaddoriya22@gmail.com>
2025-01-03 11:37:26 +0530

    admin new

--- Admin_Panel_Node/controllers/adminController.js
+++ Admin_Panel_Node/controllers/adminController.js
@@ -1,11 +1,16 @@
 const Admin = require('../models/adminModel');
 const fs = require('fs');
 const path = require('path');
+const nodemailer = require("nodemailer");
+
+// Admin CRUD Module 
 
 module.exports.dashboard = async (req, res) => {
     try {
         if (req.cookies.adminData) {
-            return res.render('dashboard');
+            return res.render('dashboard', {
+                adminData: req.cookies.adminData
+            });
         }
         else {
             return res.redirect('/')
@@ -20,7 +25,9 @@ module.exports.dashboard = async (req, res) => {
 module.exports.addAdmin = async (req, res) => {
     try {
         if (req.cookies.adminData) {
-            return res.render('AddAdmin');
+            return res.render('AddAdmin', {
+                adminData: req.cookies.adminData
+            });
         }
         else {
             return res.redirect('/')
@@ -34,13 +41,14 @@ module.exports.addAdmin = async (req, res) => {
 }
 
 module.exports.viewAdmin = async (req, res) => {
-    try {        
+    try {
         if (req.cookies.adminData) {
             let GetAdminData = await Admin.find();
             if (GetAdminData) {
                 console.log("Data found successfully")
                 return res.render('ViewAdmin', {
-                    GetAdminData
+                    GetAdminData,
+                    adminData: req.cookies.adminData
                 });
             }
             else {
@@ -124,18 +132,22 @@ module.exports.deleteAdmin = async (req, res) => {
 
 module.exports.updateAdmin = async (req, res) => {
     try {
-        let id = req.query.adminId;
-        let singleData = await Admin.findById(id);
-        if (singleData) {
-            console.log("SingleData Found ");
-            return res.render('EditAdmin', {
-                singleData
-            })
+        if (req.cookies.adminData) {
+            let id = req.query.adminId;
+            let singleData = await Admin.findById(id);
+            if (singleData) {
+                console.log("SingleData Found ");
+                return res.render('EditAdmin', {
+                    singleData,
+                    adminData: req.cookies.adminData
+                })
+            }
+            else {
+                console.log("SingleData not Found ");
+                return res.redirect('back');
+            }
         }
-        else {
-            console.log("SingleData not Found ");
-            return res.redirect('back');
-        }
+
     }
     catch (err) {
         console.log("Err", err);
@@ -146,48 +158,44 @@ module.exports.updateAdmin = async (req, res) => {
 
 module.exports.editAdmin = async (req, res) => {
     try {
-        req.body.name = req.body.fname + " " + req.body.lname
         let singleAdmin = await Admin.findById(req.body.AdminId);
+        req.body.name = req.body.fname + " " + req.body.lname
         if (req.file) {
-            if (singleAdmin) {
-                try {
-                    let deleteImgPath = path.join(__dirname, '..', singleAdmin.image);
-                    await fs.unlinkSync(deleteImgPath);
-                }
-                catch (err) {
-                    console.log("Image not Found");
-                }
 
-                var newImg = Admin.imgPath + '/' + req.file.filename;
-                req.body.image = newImg
+            try {
+                let deleteImgPath = path.join(__dirname, '..', singleAdmin.image);
+                await fs.unlinkSync(deleteImgPath);
+            }
+            catch (err) {
+                console.log("Image not Found");
+            }
 
-                let updatedData = await Admin.findByIdAndUpdate(req.body.AdminId, req.body);
-                if (updatedData) {
-                    console.log("Record Updated With Image Successfully..... ! ");
-                    return res.redirect('/viewAdmin');
-                }
-                else {
-                    console.log("Something is wrong");
-                    return res.redirect('back')
-                }
+            var newImg = Admin.imgPath + '/' + req.file.filename;
+            req.body.image = newImg
+
+        }
+        else {
+            req.body.image = singleAdmin.image
+        }
+
+        let updatedData = await Admin.findByIdAndUpdate(req.body.AdminId, req.body);
+
+        if (updatedData) {
+            let newAdmin = await Admin.findById(req.body.AdminId);
+            let oldAdmin = req.cookies.adminData;
+
+            if (oldAdmin._id === newAdmin.id) {
+                res.cookie('adminData', newAdmin);
+                return res.redirect('/viewProfile')
+            }
+            else {
+                return res.redirect('/viewAdmin');
             }
         }
         else {
-            if (singleAdmin) {
-                req.body.image = singleAdmin.image
-            }
-            let updatedData = await Admin.findByIdAndUpdate(req.body.AdminId, req.body);
-            if (updatedData) {
-                console.log("Record Updated Successfulllyyyy.....!");
-                return res.redirect('/viewAdmin');
-            }
-            else {
-                console.log("Record not Updated...........!");
-                return res.redirect('back')
-            }
-
+            console.log("Record not Updated...........!");
+            return res.redirect('back')
         }
-
     }
     catch (err) {
         console.log("Err", err);
@@ -195,6 +203,10 @@ module.exports.editAdmin = async (req, res) => {
     }
 
 }
+
+// end of Admin CRUD Module 
+
+// Sign in Module 
 
 module.exports.signIn = async (req, res) => {
     try {
@@ -239,4 +251,200 @@ module.exports.checkSignIn = async (req, res) => {
         return res.redirect('back');
     }
 
+}
+
+// end Sign in Module 
+
+// profile module 
+module.exports.viewProfile = async (req, res) => {
+    try {
+        if (req.cookies.adminData) {
+            return res.render('viewProfile', {
+                adminData: req.cookies.adminData
+            })
+        }
+        else {
+            return res.redirect('/')
+        }
+
+    }
+    catch (err) {
+        console.log("Err", err);
+        return res.redirect('back');
+    }
+}
+
+// end of profile module 
+
+// change password
+
+module.exports.changePassword = async (req, res) => {
+    try {
+        if (req.cookies.adminData) {
+            return res.render('changePassword', {
+                adminData: req.cookies.adminData
+            })
+        }
+        else {
+            return res.redirect('/')
+        }
+    }
+    catch (err) {
+        console.log("Err", err);
+        return res.redirect('back');
+    }
+
+}
+
+module.exports.changeNewPass = async (req, res) => {
+    try {
+        console.log(req.body);
+        let oldData = req.cookies.adminData;
+        if (oldData.password == req.body.currentPassword) {
+            if (req.body.currentPassword != req.body.newPassword) {
+                if (req.body.newPassword == req.body.confirmPassword) {
+                    let editPassword = await Admin.findByIdAndUpdate(oldData._id, { password: req.body.newPassword })
+                    return res.redirect('/logout');
+                }
+                else {
+                    console.log("OOpsss .... ! New Password and Confirm Password not matched !");
+                    return res.redirect('/');
+                }
+            }
+            else {
+                console.log("OOpsss .... ! Current Password and New Password not matched !");
+                return res.redirect('/');
+            }
+        }
+        else {
+            console.log("OOpsss .... ! Old Password and Current Password not matched !");
+            return res.redirect('/');
+        }
+    }
+    catch (err) {
+        console.log("Err", err);
+        return res.redirect('back');
+    }
+}
+
+module.exports.verifyEmail = async (req, res) => {
+    try {
+        console.log(req.body);
+        let singleObj = await Admin.find({ email: req.body.email }).countDocuments();
+        let OTP = Math.floor(Math.random() * 1000000);
+        if (singleObj == 1) {
+            let singleUser = await Admin.findOne({ email: req.body.email });
+
+            res.cookie('otp', OTP);
+            res.cookie('email', singleUser.email);
+
+
+            const transporter = nodemailer.createTransport({
+                host: "smtp.gmail.com",
+                port: 587,
+                secure: false, // true for port 465, false for other ports
+                auth: {
+                    user: "divupatel22199@gmail.com",
+                    pass: "msnwfbcldeuftwiz",
+                },
+                tls: {
+                    rejectUnauthorized: false
+                }
+            });
+
+            const info = await transporter.sendMail({
+                from: 'divupatel22199@gmail.com', // sender address
+                to: singleUser.email, // list of receivers
+                subject: "OTP for Varification✔", // Subject line
+                html: `<b>Your OTP is : ${OTP}</b>`, // html body
+            });
+
+            console.log("Message sent: %s", info.messageId);
+
+            return res.redirect('/checkOtp');
+
+        }
+        else {
+            console.log("Invalid Email");
+            return res.redirect('back');
+        }
+    }
+    catch (err) {
+        console.log("Err", err);
+        return res.redirect('back');
+    }
+
+}
+
+module.exports.checkOtp = async (req, res) => {
+    try {
+        return res.render('checkOtp');
+    }
+    catch (err) {
+        console.log("Err", err);
+        return res.redirect('back');
+    }
+}
+
+module.exports.verifyOtp = async (req, res) => {
+    try {
+       
+        if (req.body.otp == req.cookies.otp) {
+            res.clearCookie('otp');
+            return res.redirect('/forgotPass');
+        }
+        else {
+            console.log("Invalid OTP..Please Re-Enter your OTP");
+            return res.redirect('back');
+        }
+    }
+    catch (err) {
+        console.log("Err", err);
+        return res.redirect('back');
+    }
+}
+
+module.exports.forgotPass = async (req, res) => {
+    try {
+        return res.render('forgotPass');
+    }
+    catch (err) {
+        console.log("Err", err);
+        return res.redirect('back');
+    }
+}
+
+module.exports.verifyPass = async (req, res) => {
+    try {
+        console.log(req.body);
+        console.log(req.cookies.email);
+        if (req.body.newPass == req.body.confirmPass) {
+            let verifyEmail = await Admin.find({email:req.cookies.email}).countDocuments();
+            if(verifyEmail == 1){
+                let AdminDataNew = await Admin.findOne({email:req.cookies.email});
+                let UpdatedPass = await Admin.findByIdAndUpdate(AdminDataNew.id,{password:req.body.newPass});
+                if(UpdatedPass){
+                    res.clearCookie('email');
+                    console.log("Password Updated Successfully ...")
+                    return res.redirect('/');
+                }
+                else{
+                    console.log("Something is Wrong");
+                    return res.redirect('back');
+                }
+            }
+            else{
+                console.log("Email not Found");
+                return res.redirect('back');
+            }
+        }
+        else{
+            console.log("New Password And Confirm Password is not same !!!!!!!!!");
+            return res.redirect('back');
+        }
+    }
+    catch (err) {
+        console.log("Err", err);
+        return res.redirect('back');
+    }
 }
--- Admin_Panel_Node/routes/adminRoutes.js
+++ Admin_Panel_Node/routes/adminRoutes.js
@@ -4,18 +4,48 @@ const routes = express.Router();
 const AdminCtl = require('../controllers/adminController');
 const Admin = require('../models/adminModel');
 
-routes.get('/',AdminCtl.signIn);
-routes.post('/checkSignIn',AdminCtl.checkSignIn);
+// login routes
+routes.get('/', AdminCtl.signIn);
+routes.post('/checkSignIn', AdminCtl.checkSignIn);
+routes.get('/logout', (req, res) => {
+    res.clearCookie('adminData');
+    return res.redirect('/');
+})
+// end of login routes 
 
-routes.get('/dashboard',AdminCtl.dashboard);
+// profile routes 
+routes.get('/viewProfile',AdminCtl.viewProfile);
+// end of profile routes 
 
-routes.get('/addAdmin',AdminCtl.addAdmin);
-routes.get('/viewAdmin',AdminCtl.viewAdmin);
-routes.post('/insertAdmin',Admin.uploadAdminImg,AdminCtl.insertAdmin);
-routes.get('/deleteAdmin/:id',AdminCtl.deleteAdmin);
-routes.get('/updateAdmin',AdminCtl.updateAdmin);
-routes.post('/editAdmin',Admin.uploadAdminImg,AdminCtl.editAdmin);
+// change password
+routes.get('/changePassword',AdminCtl.changePassword);
+routes.post('/changeNewPass',AdminCtl.changeNewPass);
+// end of change password
 
+// forgot password
+routes.get('/checkEmail', (req,res)=>{
+   return res.render('checkEmail');
+});
+routes.post('/verifyEmail',AdminCtl.verifyEmail);
+routes.get('/checkOtp',AdminCtl.checkOtp);
+routes.post('/verifyOtp',AdminCtl.verifyOtp);
+routes.get('/forgotPass',AdminCtl.forgotPass);
+routes.post('/verifyPass',AdminCtl.verifyPass);
+
+// end of forgot Password
+
+// main 
+
+routes.get('/dashboard', AdminCtl.dashboard);
+
+routes.get('/addAdmin', AdminCtl.addAdmin);
+routes.get('/viewAdmin', AdminCtl.viewAdmin);
+routes.post('/insertAdmin', Admin.uploadAdminImg, AdminCtl.insertAdmin);
+routes.get('/deleteAdmin/:id', AdminCtl.deleteAdmin);
+routes.get('/updateAdmin', AdminCtl.updateAdmin);
+routes.post('/editAdmin', Admin.uploadAdminImg, AdminCtl.editAdmin);
+
+// emd of main
 
 
 module.exports = routes;
